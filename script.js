5ab157a4514936b5f0a205584fd36d511a37f5ab
Mara Schöni <mara.schoeni@gmail.com>
2023-11-06 11:27:40 +0100

    aufgabe 3

--- script.js
+++ script.js
@@ -37,5 +37,40 @@ export function aufgabe02(args) {
   return result.join("")
 }
 
-    
+
+export function aufgabe03(args) {
+  const input = args
+  const result = []
+  let count = 0
+ 
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === 'e') {
+      count++
+    } else if ( currentElement === 'E') {
+      count++
+ 
+  }
+ 
+  }
+  return count
+}
+
+export function aufgabe04(args) {
+  const input = args
+  const result = []
+  let count = 0
+ 
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === 'word') {
+      count++
+    } else if ( currentElement === 'word') {
+      count++
+ 
+  }
+ 
+  }
+  return count
+}
 
